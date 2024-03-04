2e7a87f3433d892ab66a37c641a36a1f938b3312
nadimshuvo <nadimshuvo1997@gmail.com>
2024-03-04 06:33:40 +0600

    updated

---
+++ test.js
@@ -0,0 +1,112 @@
+const problemElement = document.querySelector(".problem");
+const ourForm = document.querySelector(".our-form");
+const ourField = document.querySelector("our-field");
+const pointsNeeded = document.querySelector(".points-needed");
+const mistakesAllowed = document.querySelector(".mistakes-allowed");
+const progressBar = document.querySelector(".progress-inner");
+const endMessage = document.querySelector(".end-message");
+const resetButton = document.querySelector(".reset-button");
+
+let timer;
+let timeRemaining = 0;
+
+let state = {
+  score: 0,
+  wrongAnswers: 0,
+};
+
+function generateNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(10),
+    numberTwo: generateNumber(10),
+    operator: ["+", "-", "x"][generateNumber(2)],
+  };
+}
+
+function updateProblem() {
+  state.currentProblem = generateProblem();
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`;
+  ourField.value = "";
+  ourField.focus();
+}
+
+function checkLogic() {
+  if (state.score === 10) {
+    endMessage.textContent = "Congrats! You won !";
+    endMessage.style.color = "white";
+    document.body.classList.add("overlay-is-open");
+    document.querySelector(".overlay").style.background = "";
+    setTimeout(() => resetButton.focus(), 331);
+  } else if (state.wrongAnswers === 3) {
+    endMessage.textContent = "Sorry ! You lost !";
+    endMessage.style.color = "";
+    document.body.classList.add("overlay-is-open");
+    document.querySelector(".overlay").style.backgroundImage = "";
+    setTimeout(() => resetButton.focus(), 331);
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  let correctAnswer;
+  const p = state.currentProblem;
+  if (p.operator == "+") correctAnswer = p.numberOne + p.numberTwo;
+  if (p.operator == "-") correctAnswer = p.numberOne - p.numberTwo;
+  if (p.operator == "x") correctAnswer = p.numberOne * p.numberTwo;
+
+  if (parseInt(ourField.value, 10) === correctAnswer) {
+    state.score++;
+    pointsNeeded.textContent = 10 - state.score;
+    updateProblem();
+    renderProgressbar();
+  } else {
+    state.wrongAnswers++;
+    mistakesAllowed.textContent = 2 - state.wrongAnswers;
+    problemElement.classList.add("animate-wrong");
+    setTimeout(() => problemElement.classList.remove("animate-wrong"), 451);
+  }
+  checkLogic();
+}
+
+ourForm.addEventListener("submit", handleSubmit);
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+  clearInterval(timer);
+  document.body.classList.remove("overlay-is-open");
+  updateProblem();
+  state.score = 0;
+  state.wrongAnswers = 0;
+  pointsNeeded.textContent = 10;
+  mistakesAllowed.textContent = 2;
+  renderProgressBar();
+  timeRemaining = 30;
+
+  timer = setTimeout(() => {
+    timeRemaining--;
+    updateTimerDisplay();
+    if (timeRemaining === 0) {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+}
+
+function endGame() {
+  if (state.score !== 10) {
+    endMessage.textContent = "Sorry! You lost.";
+    endMessage.style.color = "";
+    document.body.classList.add("overlay-is-open");
+    document.querySelector(".overlay").style.backgroundImage = "";
+    setTimeout(() => resetButton.focus(), 331);
+  }
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`;
+}
